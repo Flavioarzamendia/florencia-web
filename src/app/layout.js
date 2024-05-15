ac28a9e97f3feeebf5f5bd1c734e1c3b369720cd
Flavio Arzamendia <flavioarzamendia2019@gmail.com>
2024-05-15 15:39:25 -0300

    canonical

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Florencia Astudillo - Fotógrafía Profesional',
-  description: 'Florencia Astudillo Fotografía profesional y fotoperiodismo que capturan momentos auténticos con estilo y calidad.'
+  description: 'Florencia Astudillo Fotografía profesional y fotoperiodismo que capturan momentos auténticos con estilo y calidad.',
+  canonical: 'https://florenciaastudillo.vercel.app/'
 }
 
 export default function RootLayout({ children }) {
